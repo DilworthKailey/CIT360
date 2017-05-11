dc5a50d64c42a43b78ecdefdaa974c2c921569dd
Kailey <dil11004@byui.edu>
2017-05-11 10:05:06 -0600

    5/10/17 - hash sets

---
+++ CIT360 Sandbox/src/JavaCollections/HashSetExample.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaCollections;
+import java.util.*;
+
+public class HashSetExample {
+
+    public static void main(String[] args) {
+
+        String [] things = {"banana", "orange", "apple", "lettuce", "turnip"};
+        List<String> list = Arrays.asList(things);
+
+        System.out.printf("%s ", list);
+        System.out.println();
+
+        Set<String> set = new HashSet<>(list);
+        System.out.printf("%s ", set);  
+    }
+
+}
+
