04ce0756342ab79a8c4256ddb95fbc8cf3cf8de3
Kailey <dil11004@byui.edu>
2017-05-25 19:37:22 -0600

    5/25/17 - Finished Calculator -, *, and /

--- CIT360 Sandbox/src/MVCExample/CalculatorModel.java
+++ CIT360 Sandbox/src/MVCExample/CalculatorModel.java
@@ -6,7 +6,7 @@ package MVCExample;
  */
 
 public class CalculatorModel {
-
+        private CalculatorView theView;
 	// Holds the value of the sum of the numbers
 	// entered in the view
 	
@@ -17,7 +17,20 @@ public class CalculatorModel {
 		calculationValue = firstNumber + secondNumber;
 		
 	}
-	
+	public void subtractTwoNumbers(int thirdNumber, int fourthNumber){
+		
+		calculationValue = thirdNumber - fourthNumber;
+		
+	}
+        public void multiplyTwoNumbers(int fifthNumber, int sixthNumber){
+		
+		calculationValue = fifthNumber * sixthNumber;
+		
+	}
+        public void divideTwoNumbers(int seventhNumber, int eigthNumber){		
+                    calculationValue = seventhNumber / eigthNumber;
+			
+	}
 	public int getCalculationValue(){
 		
 		return calculationValue;
