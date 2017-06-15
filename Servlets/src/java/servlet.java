a8eda7afeefedb35a3edb18b0549f86108d31fbf
Kailey <dil11004@byui.edu>
2017-06-15 13:16:03 -0600

    6/15/17 - Servlets Draft 1

--- Servlets/src/java/servlet.java
+++ Servlets/src/java/servlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author Josh
+ * @author Kailey
  */
 @WebServlet(urlPatterns = {"/servlet"})
 public class servlet extends HttpServlet {
